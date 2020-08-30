881614d1cdb6537d8cb6dba4b7119ea285ac8b9e
Antal Nemes <antal.nemes.hu@gmail.com>
2020-08-30 10:59:48 +0200

    give the lambdas proper name
    Signed-off-by: Antal Nemes <antal.nemes.hu@gmail.com>

--- tests/test_pyducers.py
+++ tests/test_pyducers.py
@@ -1,6 +1,18 @@
 from pyducers import filter, map, transduce
 
 
+def add(*args):
+    return sum(args)
+
+
+def square(n):
+    return n*n
+
+
+def two_times(n):
+    return 2*n
+
+
 def assert_iter_eq(iter1, iter2):
     assert list(iter1) == list(iter2)
 
@@ -9,13 +21,12 @@ def test_filter():
     def evenp(num):
         return num % 2 == 0
 
-    assert 6 == transduce(filter(evenp), lambda x, y: x+y, [1, 2, 3, 4], 0)
+    assert 6 == transduce(filter(evenp), add, [1, 2, 3, 4], 0)
 
 
 def test_map():
-    assert_iter_eq([2, 4, 6], map(lambda x: 2*x, [1, 2, 3]))
-    assert_iter_eq([2, 3, 4], map(lambda *x: sum(x),
-                                  [1, 1, 1], [1, 0, 0], [0, 2, 0], [0, 0, 3]))
+    assert_iter_eq([2, 4, 6], map(two_times, [1, 2, 3]))
+    assert_iter_eq([2, 3, 4], map(add, [1, 1, 1],
+                                  [1, 0, 0], [0, 2, 0], [0, 0, 3]))
 
-    assert 1*1 + 2*2 + 3*3 == transduce(map(lambda x: x*x), lambda x, y: x + y,
-                                        [1, 2, 3], 0)
+    assert 1*1 + 2*2 + 3*3 == transduce(map(square), add, [1, 2, 3], 0)
