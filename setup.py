6696d9b3657f9187e1eb0b7db0d72c72e18c0403
Antal Nemes <antal.nemes.hu@gmail.com>
2020-08-30 22:03:39 +0200

    version from file, MANIFEST.in
    Signed-off-by: Antal Nemes <antal.nemes.hu@gmail.com>

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+with open("VERSION", "r") as fh:
+    version = fh.read()
 
 setuptools.setup(
     name="pyducers-furiel",
-    version="0.0.1",
+    version=version,
     author="furiel",
     author_email="antal.nemes@gmail.com",
     description="Transducer library for python inspired by clojure",
